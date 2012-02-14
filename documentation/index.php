b0157909edc01bf965a75039fc784349c6fcd777
smcculloch <smcculloch>
2012-02-14 16:52:56 +0000

    First draft of the Sisu project website, based on the Aether project layout.

---
+++ documentation/index.php
@@ -0,0 +1,44 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+/*******************************************************************************
+ * Copyright (c) 2012 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    
+ *******************************************************************************/
+
+$pageTitle 		= $pageTitle . " - Documentation";
+
+$gitRepos = $project->getSourceRepositories(); 
+
+$html  = <<<EOHTML
+<div id="midcolumn">
+<h2>$pageTitle</h2>
+
+<h3 id="apidocs">API Documentation</h3>
+<p>
+  For technical details regarding usage of the API, please consult the
+  generated <a href="$apidocs">API docs</a> of the various classes.
+</p>
+<p>
+  These API docs are also available from the Central Repository such that IDEs with Maven integration can present
+  you with that information automatically when you edit your code. 
+</p>
+
+<h3 id="wiki">Wiki</h3>
+<p>
+  More high-level and task-focused documentation regarding $projectName's usage can be found in our
+  <a href="$wiki">wiki space</a>.
+</p>
+
+<div id="rightcolumn">
+$incubation
+</div>
+EOHTML;
+
+# Generate the web page
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009 Eclipse Foundation and others.
+ * Copyright (c) 2011, 2012 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,44 @@
  *    
  *******************************************************************************/
 
-header("Location: http://www.eclipse.org/projects/project.php?id=technology.sisu");
+$html  = <<<EOHTML
+<div id="bigbuttons">
+	<h3>Primary Links</h3>
+	<ul>
+		<li><a id="buttonDownload" href="download/" title="Download">Eclipse Distribution, Update Site</a></li>
+		<li><a id="buttonDocumentation" href="documentation/" title="Documentation">Tutorials, Examples, Reference Documentation</a></li>
+		<li><a id="buttonSupport" href="support/" title="Support">Bug Tracker, Newsgroup, Support</a></li>
+		<li><a id="buttonInvolved" href="developers/" title="Getting Involved">VCS, Workspace Setup, Wiki, Committers</a></li>
+	</ul>
+</div>
 
+<div id="midcolumn">
+<h2>$pageTitle</h2>
+<p>
+  $projectDescription
+</p>
+</div>
+
+<div id="rightcolumn">
+$incubation
+
+<div class="sideitem">
+<h6>Resources</h6>
+<ul>
+  <li><a href="$about">About $projectName</a></li>
+  <li><a href="developers/#sources">Sources</a></li>
+  <li><a href="documentation/#apidocs">API Docs</a></li>
+  <li><a href="support/#issues">Issues</a></li>
+  <li><a href="$wiki">Wiki</a></li>
+  <li><a href="http://www.eclipse.org/legal/epl-v10.html">License</a></li>
+</ul>
+</div>
+</div>
+
+EOHTML;
+
+$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>' . "\n\t");
+
+# Generate the web page
+$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
