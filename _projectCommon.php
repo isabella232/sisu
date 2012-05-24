fa01545c6a743b15dde0be8d890b95a1835f2180
smcculloch <smcculloch>
2012-05-24 01:05:07 +0000

    Fix slashes

--- _projectCommon.php
+++ _projectCommon.php
@@ -47,26 +47,26 @@ $pageTitle 		= $projectName;
 $Nav->setLinkList(array());
 
 # Break the navigation into sections
-$Nav->addNavSeparator($projectName, $projectUrl . "/");
+$Nav->addNavSeparator($projectName, $projectUrl);
 $Nav->addCustomNav("About", "/projects/project.php?id=$projectId", "_self", 3);
-$Nav->addCustomNav("Downloads", $projectUrl . "/download/", "_self", 3);
-$Nav->addCustomNav("Documentation", $projectUrl . "/documentation/", "_self", 3);
-$Nav->addCustomNav("Support", $projectUrl . "/support/", "_self", 3);
-$Nav->addCustomNav("Developers", $projectUrl . "/developers/", "_self", 3);
+$Nav->addCustomNav("Downloads", $projectUrl . "download/", "_self", 3);
+$Nav->addCustomNav("Documentation", $projectUrl . "documentation/", "_self", 3);
+$Nav->addCustomNav("Support", $projectUrl . "support/", "_self", 3);
+$Nav->addCustomNav("Developers", $projectUrl . "developers/", "_self", 3);
 
 # top navigation bar
 # To override and replace the navigation with your own, uncomment the line below.
 $Menu->setMenuItemList(array());
-$Menu->addMenuItem("Home", $projectUrl . "/", "_self");
-$Menu->addMenuItem("Downloads", $projectUrl . "/download/", "_self");
-$Menu->addMenuItem("Documentation", $projectUrl . "/documentation/", "_self");
-$Menu->addMenuItem("Support", $projectUrl . "/support/", "_self");
-$Menu->addMenuItem("Developers", $projectUrl . "/developers/", "_self");
+$Menu->addMenuItem("Home", $projectUrl, "_self");
+$Menu->addMenuItem("Downloads", $projectUrl . "download/", "_self");
+$Menu->addMenuItem("Documentation", $projectUrl . "documentation/", "_self");
+$Menu->addMenuItem("Support", $projectUrl . "support/", "_self");
+$Menu->addMenuItem("Developers", $projectUrl . "developers/", "_self");
 
 # To define additional CSS or other pre-body headers
 # FIXME: Both CSS files cause the rightcolumn to be improperly placed/rendered
 # $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/default/style.css"/>');
-# $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="' . $projectUrl . '/style.css"/>');
+# $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="' . $projectUrl . 'style.css"/>');
 
 # To enable occasional Eclipse Foundation Promotion banners on your pages (EclipseCon, etc)
 $App->Promotion = TRUE;
