17ae358608af02309d77b29381f29203e8f7a219
Stuart McCulloch <mcculls@gmail.com>
2013-07-04 00:56:25 +0100

    Bring over Sisu customizations

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,7 +1,6 @@
 <?php
-
 /*******************************************************************************
- * Copyright (c) 2009 Eclipse Foundation and others.
+ * Copyright (c) 2011, 2012 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,47 +10,65 @@
  *    
  *******************************************************************************/
 
-	# Set the theme for your project's web pages.
-	# See http://eclipse.org/phoenix/
-	$theme = "Nova";
-	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/classes/Project.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/classes/ProjectSummary.class.php");
 
-	# Define your project-wide Navigation here
-	# This appears on the left of the page if you define a left nav
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# these are optional
-	
-	# If you want to override the eclipse.org navigation, uncomment below.
-	# $Nav->setLinkList(array());
-	
-	# Break the navigation into sections
-	$Nav->addNavSeparator("Sisu", 	"/sisu");
-	$Nav->addCustomNav("Download", "/project/download.php", "_self", 3);
-	$Nav->addCustomNav("Documentation", "/project/documentation.php", "_blank", 3);
-	$Nav->addCustomNav("Support", "/project/support.php", "_blank", 3);
-	$Nav->addCustomNav("Getting Involved", "/project/developers", "_blank", 3);
+$projectId = "technology.sisu";
+$project = getProject($projectId);
+$summary = new ProjectSummary($project);
+$projectName = $project->getName();
+$projectShortName = $project->getShortName();
+$projectDescription = $project->getDescription();
+$projectUrl = normalizeRelativeUrl($project->getProjectUrl());
+$issueTracker = normalizeHttpUrl($project->getBugzillaUrl()); 
+$wiki = normalizeHttpUrl($project->getWikiUrl());
+$sources = "http://git.eclipse.org/c/sisu/";
+$ci = "https://hudson.eclipse.org/hudson/";
+$apidocs = "http://download.eclipse.org/sisu/org.eclipse.sisu.inject/current/apidocs/";
+$about = "/projects/project.php?id=$projectId";
+$iplog = "/projects/ip_log.php?projectid=$projectId";
+$incubation = $summary->getIncubationSideItem();
 
-	# Define keywords, author and title here, or in each PHP page specifically
-	$pageKeywords	= "sisu, dependency injection, inject, ioc, jsr330";
-	$pageAuthor	= "Stuart McCulloch";
-	# $pageTitle 	= "Sisu";
+# Set the theme for your project's web pages.
+# See http://eclipse.org/phoenix/
+$theme = "Nova";
 
+# Define keywords, author and title here, or in each PHP page specifically
+$pageKeywords	= "sisu, dependency injection, inject, ioc, jsr330";
+$pageAuthor	= "Stuart McCulloch";
+$pageTitle 	= $projectName;
 
-	# top navigation bar
-	# To override and replace the navigation with your own, uncomment the line below.
-	# $Menu->setMenuItemList(array());
-	# $Menu->addMenuItem("Home", "/project", "_self");
-	# $Menu->addMenuItem("Download", "/project/download.php", "_self");
-	# $Menu->addMenuItem("Documentation", "/project/documentation.php", "_self");
-	# $Menu->addMenuItem("Support", "/project/support.php", "_self");
-	# $Menu->addMenuItem("Developers", "/project/developers", "_self");
-	
-	# To define additional CSS or other pre-body headers
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/sisu/style.css"/>');
-	
-	# To enable occasional Eclipse Foundation Promotion banners on your pages (EclipseCon, etc)
-	$App->Promotion = FALSE;
-	
-	# If you have Google Analytics code, use it here
-	# $App->SetGoogleAnalyticsTrackingCode("YOUR_CODE");
+# Define your project-wide Navigation here
+# This appears on the left of the page if you define a left nav
+# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+# these are optional
+
+# If you want to override the eclipse.org navigation, uncomment below.
+$Nav->setLinkList(array());
+
+# Break the navigation into sections
+$Nav->addNavSeparator("$projectName Home", $projectUrl);
+$Nav->addCustomNav("Documentation", $projectUrl . "docs/", "_self", 3);
+$Nav->addCustomNav("Downloads", $projectUrl . "download/", "_self", 3);
+$Nav->addCustomNav("Getting Involved", $projectUrl . "developers/", "_self", 3);
+$Nav->addCustomNav("Support", $projectUrl . "support/", "_self", 3);
+$Nav->addCustomNav("About Sisu", "/projects/project.php?id=$projectId", "_self", 3);
+
+# top navigation bar
+# To override and replace the navigation with your own, uncomment the line below.
+# $Menu->setMenuItemList(array());
+# $Menu->addMenuItem("Home", $projectUrl, "_self");
+# $Menu->addMenuItem("Documentation", $projectUrl . "docs/", "_self");
+# $Menu->addMenuItem("Downloads", $projectUrl . "download/", "_self");
+# $Menu->addMenuItem("Getting Involved", $projectUrl . "developers/", "_self");
+# $Menu->addMenuItem("Support", $projectUrl . "support/", "_self");
+
+# To define additional CSS or other pre-body headers
+$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="' . $projectUrl . 'style.css"/>');
+
+# To enable occasional Eclipse Foundation Promotion banners on your pages (EclipseCon, etc)
+$App->Promotion = FALSE;
+
+# If you have Google Analytics code, use it here
+# $App->SetGoogleAnalyticsTrackingCode("YOUR_CODE");
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009 Eclipse Foundation and others.
+ * Copyright (c) 2011, 2012 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,20 +10,52 @@
  *    
  *******************************************************************************/
 
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());
-	
-	$localVersion = false;
-	
-	# Define these here, or in _projectCommon.php for site-wide values
-	# $pageKeywords	= "eclipse, project";
-	# $pageAuthor		= "Your name";
-	$pageTitle 		= "Sisu";
-	
-	
-	// 	# Paste your HTML content between the EOHTML markers!
-	$html = file_get_contents('pages/_index.html');
+$html = <<<EOHTML
+<span class="sisu">
+  <div class="sisu-0"></div>
+  <div class="sisu-1 sisu-dot"></div>
+  <div class="sisu-2"></div>
+  <div class="sisu-3"></div>
+  <div class="sisu-4 sisu-dot"></div>
+  <div class="sisu-5"></div>
+  <div class="sisu-6">
+    <div class="sisu-1 sisu-dot"></div>
+    <div class="sisu-2"></div>
+    <div class="sisu-3"></div>
+    <div class="sisu-4 sisu-dot"></div>
+    <div class="sisu-5"></div>
+    <div class="sisu-7"></div>
+    <div class="sisu-8 sisu-dot"></div>
+  </div>
+</span>
+<div id="bigbuttons">
+<h3>Primary Links</h3>
+<ul>
+  <li><a id="buttonDocumentation" href="docs/" title="Documentation">
+  <div class="buttonIcon"></div><h4>Documentation</h4>
+  <p>Tutorials, Javadoc, Reference Documentation</p></a></li>
+  <li><a id="buttonDownload" href="download/" title="Download">
+  <div class="buttonIcon"></div><h4>Download</h4>
+  <p>Distribution, Update Site</p></a></li>
+  <li><a id="buttonInvolved" href="involved/" title="Getting Involved">
+  <div class="buttonIcon"></div><h4>Getting Involved</h4>
+  <p>Sources, Workspace Setup, Wiki, Committers</p></a></li>
+  <li><a id="buttonSupport" href="support/" title="Support">
+  <div class="buttonIcon"></div><h4>Support</h4>
+  <p>Bug Tracker, Mailing Lists, Newsgroup</p></a></li>
+</ul>
+</div>
+<div id="midcolumn">
+  <h2>$pageTitle</h2>
+  <p>
+    $projectDescription
+  </p>
+</div>
+<div id="rightcolumn">
+  $incubation
+</div>
+EOHTML;
 
-	# Generate the web page
-	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
-
+# Generate the web page
+$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
