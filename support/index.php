7771ce7314952fa38e423953ddde62a24eed9d1f
Stuart McCulloch <mcculls@gmail.com>
2013-07-04 01:06:38 +0100

    More Sisu details

--- support/index.php
+++ support/index.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009 
+ * Copyright (c) 2012 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,24 +10,42 @@
  *    
  *******************************************************************************/
 
-	$pageTitle 		= "MyProject - Download";
+$pageTitle 		= $pageTitle . " - Support";
 
-	$html  = <<<EOHTML
+$mailingList  = "sisu-users";
+$issueTracker = normalizeHttpUrl($project->getBugzillaUrl()); 
+
+$html  = <<<EOHTML
 <div id="midcolumn">
 <h2>$pageTitle</h2>
-<p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
 
-<h3>Galileo - Eclipse 3.5</h3>
-<p><b>Update site:</b> http://download.eclipse.org/myproject/<br />
-<b>ZIP file: </b><a href="/downloads/download.php?file=/myproject/file.zip">file.zip</a> (10 MiB)</p>
+<h3 id="mail">Mailing List</h3>
+<p>
+  If you have a question that the existing documentation does not answer, feel free to contact our
+  <a href="http://dev.eclipse.org/mailman/listinfo/$mailingList">user mailing list</a>.
+</p>
+<p>
+  Before posting your question, please also 
+  <a href="http://dev.eclipse.org/mhonarc/lists/$mailingList/">search the mailing list archive</a>
+  to see whether your question has been brought up before and was already answered.
+</p>
 
-<h3>Helios - Eclipse 3.6 (unreleased)</h3>
-<p><b>Update site:</b> http://download.eclipse.org/myproject/<br />
-<b>ZIP file: </b><a href="/downloads/download.php?file=/myproject/file.zip">file.zip</a> (10 MiB)</p>
+<h3 id="issues">Issue Tracker</h3>
+<p>
+  If you encountered a bug or have a feature request, please fill a sufficiently detailed issue in our
+  <a href="$issueTracker">issue tracker</a>.
+  Ideally, your description and attached logs should enable us to reproduce your issue.
+</p>
+<p>
+  Please also take the time to check whether your issue has already been filled by someone else. 
+</p>
+</div>
 
-
+<div id="rightcolumn">
+$incubation
 </div>
 EOHTML;
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+
+# Generate the web page
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
