2279479a43cb52a8c4e9cdf3c0b64813c0a21ff0
Stuart McCulloch <mcculls@gmail.com>
2013-11-12 21:46:48 +0000

    Update sidebar

--- _projectCommon.php
+++ _projectCommon.php
@@ -52,7 +52,8 @@ $Nav->addCustomNav("Documentation", $projectUrl . "docs/", "_self", 3);
 $Nav->addCustomNav("Downloads", $projectUrl . "download/", "_self", 3);
 $Nav->addCustomNav("Getting Involved", $projectUrl . "developers/", "_self", 3);
 $Nav->addCustomNav("Support", $projectUrl . "support/", "_self", 3);
-$Nav->addCustomNav("Roadmap", "/projects/project-plan.php?projectid=$projectId", "_self", 3);
+$Nav->addCustomNav("Changelog", "http://wiki.eclipse.org/Sisu/Changelog", "_self", 3);
+$Nav->addCustomNav("Roadmap", "/projects/$projectId/releases", "_self", 3);
 $Nav->addCustomNav("About Sisu", "/projects/project.php?id=$projectId", "_self", 3);
 
 # top navigation bar
